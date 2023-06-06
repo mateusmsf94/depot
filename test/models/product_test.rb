365a296b277ef3cd089e4e80956707b72a0e21df
Mateus Figueiredo <mateusmelosf@gmail.com>
2023-06-05 23:54:00 -0300

    validations

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,13 @@
-require "test_helper"
+require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :products
+
+  test 'product is not valid without a unique title' do
+    product = Product.new(title: products(:ruby).title,
+                          description: 'yyy',
+                          price: 1)
+    assert product.invalid?
+    assert_equal ['has already been taken'], product.errors[:title]
+  end
 end
